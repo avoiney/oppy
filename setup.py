44c11544633e068ff2f4b277b3690ca237a67f9c
Alex V <1880968+avoiney@users.noreply.github.com>
2020-05-06 16:53:42 +0200

    Bump version 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='OpPy',
-    version='0.1.0',
+    version='0.1.1',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/avoiney/oppy',
